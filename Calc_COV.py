fixed the MCMC and update QS to include the error covariance model of surrogate modelling

Calc_COV.py
@@ -93,7 +93,7 @@ def calc_likelihood(dist,s,modelObj,cand_index,ti,**kwargs):
         if modelObj.LikeProb==1:
             # calculating the covariance function based on real training image parameters
             COV_tot = cov_calc(pairs,modelObj.a0,np.power(modelObj.sigma_m,2))
-        if modelObj.LikeProb==2:
+        if modelObj.LikeProb==2 or modelObj.LikeProb==4:
             # calculating the covariance function using the estimated parameters from the variogram
             COV_tot = cov_calc(pairs,modelObj.est_a0,modelObj.est_var,alpha=modelObj.est_alpha)
         elif (modelObj.LikeProb==3):
@@ -145,6 +145,9 @@ def calc_likelihood(dist,s,modelObj,cand_index,ti,**kwargs):
         Sigma_L = np.delete(Sigma_L,modelObj.index, axis=0)
         Sigma_L = np.delete(Sigma_L,modelObj.index, axis=1)
     misf = modelObj.d_obs - mu_L
+    if modelObj.LikeProb==4:
+        Sigma_L += modelObj.C_Tapp
+        misf -= modelObj.d_Tapp
     # logP = -( modelObj.d_obs.size / 2.0) * np.log(2.0 * np.pi) - 0.5 * np.linalg.det(Sigma_L_inv) - 0.5 * (np.matmul(np.matmul(misf.transpose(0,2,1),Sigma_L_inv),misf))
     logP = -( modelObj.d_obs.size / 2.0) * np.log(2.0 * np.pi) - 0.5 * np.linalg.slogdet(Sigma_L)[1] - 0.5 * (np.matmul(np.matmul(misf.transpose(0,2,1),np.linalg.inv(Sigma_L)),misf))
     # print('likelihood calculation time'+str((time.time()-start)))

Model_obj.py
@@ -38,5 +38,6 @@ class Model_obj:
     fw: str = None;
     param: np.float32 = field(default=None);
     tt: str = None
-
+    d_Tapp: np.float32 = None
+    C_Tapp: np.float32 = None
     

qs.py
@@ -131,11 +131,26 @@ if __name__ == "__main__":
         model.fw = "linear"
     else:
         d_obs, param, index = pygimli_fw(true_model, bh_spacing=x/10, bh_length=y/10, sensor_spacing=0.4,sigma_d=sigma_d,limit_angle=1,dir=outdir)
-        if args.resim:
+        if args.LikeProb==4:
+            d_lin, A_lin, index_lin = set_fw(y,x,s_model=true_model,loc=0,scale=sigma_d,spacing=0.1,SnR_spacing=4,limit_angle=1,dir=outdir)
+            model = Model_obj(data_cond,args.LikeProb,args.sampProp,x,y,sigma_d,sigma_m,mu_m,A_lin,d_obs)
+            with open('mean_cov_error_channels.pkl', 'rb') as f:
+                tmp = pickle.load(f)
+            d_Tapp = tmp[0]
+            C_Tapp = tmp[1]
+            if index.size>0:
+                d_Tapp = np.delete(d_Tapp,index, axis=0)
+                C_Tapp = np.delete(C_Tapp,index, axis=0)
+                C_Tapp = np.delete(C_Tapp,index, axis=1)
+            model.d_Tapp = d_Tapp
+            model.C_Tapp = C_Tapp
+            model.fw = "linear"
+            del d_lin, index_lin, tmp, d_Tapp, C_Tapp
+        elif args.resim:
             from sim import set_J
             from Calc_COV import update_J
-            bestModel = np.load(outdir+'bestModel.npy')
-            # bestModel = true_model
+            # bestModel = np.load(outdir+'bestModel.npy')
+            bestModel = true_model
             temp_tt = set_J(param,bestModel)
             A=update_J(bestModel, temp_tt)
             model = Model_obj(data_cond,args.LikeProb,args.sampProp,x,y,sigma_d,sigma_m,mu_m,A,d_obs)